4ebbd7ccff3ae587b1cff747d022caf8788227e1
SeungHyeon <namsh1125@naver.com>
2024-01-20 13:11:16 +0900

    Init: 스프링 초기 셋팅

---
+++ src/test/java/gcu/kea/terraform/TerraformApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.kea.terraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
